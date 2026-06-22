8d141241289f8be2b68917fac38b621a750adbd9
Mateus <130506942+mateusmf4@users.noreply.github.com>
2026-06-22 19:23:38 -0300

    fix: adicionar cors

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { routes as usuarioRoutes } from './modules/Usuario/UsuarioRoute.js';
 import { despesaRoutes } from './modules/Despesa/DespesaRoute.js';
 import { receitaRoutes } from './modules/Receita/ReceitaRoute.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/usuarios', usuarioRoutes);
 app.use('/despesas', despesaRoutes);
