59e4d1bd4ddfdd22a1013a4daa1582b69ad645e5
FLuiz22 <filipeluiz39@gmail.com>
2026-07-04 23:59:29 -0300

    hotfix: definição da origem do cors

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,11 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+    }),
+);
 
 app.use('/usuarios', usuarioRoutes);
 app.use('/despesas', despesaRoutes);
